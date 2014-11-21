0dd02b2f75e9c863df49afc4aead52b16ba2f9f1
Jarek <jarek@phalcon.ovh>
2014-11-21 16:45:29 +0100

    Update PmController.php

--- app/controllers/PmController.php
+++ app/controllers/PmController.php
@@ -36,8 +36,7 @@ class PmController extends ControllerBase
         $this->tag->setTitle('Private Message');
         $userId = $this->session->get('identity');
 
-        if ($userId !='')
-        {
+        if ($userId !='') {
 	    $phql = "SELECT *, pm.id as privID, users.name FROM pm INNER JOIN users ON users.id = pm.sender WHERE pm.to='{$userId}' AND folder='0'";
 
             $this->view->pm = $this->db->fetchAll($phql, \Phalcon\Db::FETCH_OBJ);
@@ -53,8 +52,7 @@ class PmController extends ControllerBase
             $this->tag->setTitle('Private Message - Starred');
             $userId = $this->session->get('identity');
 
-        if ($userId !='')
-        {
+        if ($userId !='') {
             $phql = "SELECT *, pm.id as privID, users.name FROM pm INNER JOIN users ON users.id = pm.sender WHERE pm.to='{$userId}' AND folder='1'";
 
             $this->view->pm = $this->db->fetchAll($phql, \Phalcon\Db::FETCH_OBJ);
@@ -71,8 +69,7 @@ class PmController extends ControllerBase
             $this->tag->setTitle('Private Message - Important');
             $userId = $this->session->get('identity');
 
-        if ($userId !='')
-        {
+        if ($userId !='') {
             $phql = "SELECT *, pm.id as privID, users.name FROM pm INNER JOIN users ON users.id = pm.sender WHERE pm.to='{$userId}' AND folder='2'";
 
             $this->view->pm = $this->db->fetchAll($phql, \Phalcon\Db::FETCH_OBJ);
@@ -89,8 +86,7 @@ class PmController extends ControllerBase
             $this->tag->setTitle('Private Message - Sent');
             $userId = $this->session->get('identity');
 
-        if ($userId !='')
-        {
+        if ($userId !='') {
             $phql = "SELECT *, pm.id as privID, users.name FROM pm INNER JOIN users ON users.id = pm.sender WHERE pm.to='{$userId}' AND folder='3'";
 
             $this->view->pm = $this->db->fetchAll($phql, \Phalcon\Db::FETCH_OBJ);
@@ -107,8 +103,7 @@ class PmController extends ControllerBase
             $this->tag->setTitle('Private Message - Drafts');
             $userId = $this->session->get('identity');
 
-        if ($userId !='')
-        {
+        if ($userId !='') {
             $phql = "SELECT *, pm.id as privID, users.name FROM pm INNER JOIN users ON users.id = pm.sender WHERE pm.to='{$userId}' AND folder='4'";
 
             $this->view->pm = $this->db->fetchAll($phql, \Phalcon\Db::FETCH_OBJ);
@@ -124,8 +119,7 @@ class PmController extends ControllerBase
     {	
             $userId = $this->session->get('identity');
 
-        if ($userId !='')
-        {
+        if ($userId !='') {
             $get_message = "SELECT *, pm.id as privID, users.gravatar_id as gravatar, users.name FROM pm INNER JOIN users ON users.id = pm.sender WHERE pm.to='{$userId}' AND pm.id='{$id}'";
             $this->view->message = $this->db->fetchAll($get_message, \Phalcon\Db::FETCH_OBJ);
 		
